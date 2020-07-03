3d1270618afce350f74f3478ccabdc781977e7e3
EliTsao <elitsao.y@gmail.com>
2020-07-03 23:05:24 +0800

    the forth push of my blog

--- article/views.py
+++ article/views.py
@@ -10,3 +10,13 @@ def article_list(request):
     articles = ArticlePost.objects.all()
     context = {'articles':articles}
     return  render(request, 'article/list.html', context)
+
+
+# 文章详情
+def article_detail(request, id):
+    # 取出相应的文章
+    article = ArticlePost.objects.get(id=id)
+    # 需要传递给模板的对象
+    context = {'article':article}
+    # 载入模板，并返回context
+    return render(request,'article/detail.html',context)
