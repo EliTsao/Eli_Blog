40112c505c03c0b108210e389414a145ae6fd643
EliTsao <elitsao.y@gmail.com>
2020-07-04 19:02:31 +0800

    the ninth push of my blog

--- userprofile/urls.py
+++ userprofile/urls.py
@@ -6,4 +6,6 @@ app_name = 'userprofile'
 urlpatterns = [
     path('login/', views.user_login, name='login'),
     path('logout/', views.user_logout, name='logout'),
+    # 用户注册
+    path('register/', views.user_register, name='register'),
 ]
