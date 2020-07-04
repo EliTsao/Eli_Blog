bbe96ae071850b3b07ae7d53ef28d7bd30eeed8b
EliTsao <elitsao.y@gmail.com>
2020-07-04 17:38:53 +0800

    the eighth push of my blog

--- blog/urls.py
+++ blog/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     # 新增代码，配置app的url
     path('article/', include('article.urls', namespace='article')),
+    path('userprofile/', include('userprofile.urls', namespace='userprofile')),
 ]
